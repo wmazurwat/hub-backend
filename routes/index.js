93625f09ab9aab07de4918c974fa91822c2b2458
wmazurwat <wojciech.mazur.lo@gmail.com>
2022-11-04 22:05:09 +0100

    api conection

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/yt', function(req, res, next) {
-  console.log(req.body.url)
-  ytdl(req.body.url)
-    .pipe(fs.createWriteStream('video.mp4'))
-  res.sendFile('video.mp4', { root: '.' });
+router.post('/yt', async (req, res, next) => {
+
+  await ytdl(req.body.url)
+    .pipe(fs.createWriteStream('video2.mp4'))
+  res.sendFile('video2.mp4', { root: '.' });
 });
+
+router.get('/yt', async (req, res, next) => {
+  // console.log(req.body.url)
+  // await ytdl('https://www.youtube.com/watch?v=hDMg6lBsCAU')
+  //   .pipe(fs.createWriteStream('video2.mp4'))
+  res.sendFile('video2.mp4', { root: '.' });
+});
+
+
+
+router.post('/yt-info', async (req, res, next) => {
+  let info = await ytdl.getInfo(req.body.url);
+  let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+  res.send(audioFormats);
+});
+
+
 
 
 module.exports = router;
